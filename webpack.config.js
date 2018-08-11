5bee80628fae754c390e445b8e607bf63610aa78
Phuong Luu Hoang <phuonghoang.work@gmail.com>
2018-08-11 08:55:39 -0400

    [Issue 6] Added useful plugins for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: ["./src/index.js"],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
   },
+  devtool: "source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Weight Tracker",
+      // Load a custom template (lodash by default see the FAQ for details)
+      template: "./src/index.html"
+    })
+  ]
 };
