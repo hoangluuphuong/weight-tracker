294d5308f21b3bd887458ab1e4c91bd7e871b6f9
Phuong Luu Hoang <phuonghoang.work@gmail.com>
2018-09-05 12:27:01 -0400

    Add a temporary view of the front page

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/style.css';
+// import logo from './images/logo.jpeg';
+
+// const logoImg = document.getElementById('logo');
+// logoImg.src = logo;
+
+function showSliderImage(imageIndex) {
+
+}
+
+const slideIndex = 0;
+showSliderImage(slideIndex);
